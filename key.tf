ca39c479ae6a34dd1bdb74a63f9c3922fbb18aed
kendohopkins <95763576+Kendohopkins@users.noreply.github.com>
2024-04-12 17:59:30 +0200

    Update key.tf

--- key.tf
+++ key.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "rr-tf" {
   key_name   = "key-tf"
-  public_key = file("C:\Users\HP\Downloads")
+  public_key = file("C:/Users/HP/Downloads/key-tf")
 }
 
 resource "tls_private_key" "rr" {
@@ -8,10 +8,8 @@ resource "tls_private_key" "rr" {
   rsa_bits  = 4096
 }
 
-resource "local_sensitive_file" "key" {
+resource "local_file" "key" {
   depends_on = [tls_private_key.rr]
   content    = tls_private_key.rr.private_key_pem
   filename   = "i-key"
-
-
 }
